b343938167b6a2694cf6e8fd0a160e05d1cbd6c6
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-05-31 23:08:21 +0900

    猫が矢をよけるゲーム 猫と背景を配置 猫の左右移動

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            transform.Translate (-3, 0, 0); //move to left
+        }
+        if (Input.GetKeyDown (KeyCode.RightArrow)) {
+            transform.Translate (3, 0, 0); //move to right
+        }
+    }
+}
